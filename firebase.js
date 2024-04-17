80288edf896166cfe527cee4a3f26fa248fd1d37
Vansh Gupta <97226346+goyalvansh4@users.noreply.github.com>
2024-04-17 15:17:19 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB5wYKcU1irQotyq9YSbrEhA0VNkGJHDks",
+    authDomain: "my--first-firebase-project.firebaseapp.com",
+    projectId: "my--first-firebase-project",
+    storageBucket: "my--first-firebase-project.appspot.com",
+    messagingSenderId: "1077442568712",
+    appId: "1:1077442568712:web:3ccad7c10e5d98552a67bd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
